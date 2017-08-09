e1d92317ff096aad4cb73099f3407a88ca070319
mariana <salinasmariana36@mail.com>
2017-08-08 23:30:15 -0430

    conexion bd

--- db/class.db.php
+++ db/class.db.php
@@ -1,9 +1,9 @@
 <?php
-define( 'DB_HOST', 'localhost' ); 
-define( 'DB_USER', 'root' ); 
-define( 'DB_PASS', '' );
-define( 'DB_NAME', 'hydra' ); 
+define( 'DB_HOST', '138.117.149.57' );
+define( 'DB_USER', 'hydrasen_user' );
+define( 'DB_PASS', 'Lw=!9E9ke9Ht' );
+define( 'DB_NAME', 'hydrasen_hydra' ); 
 define( 'SEND_ERRORS_TO', 'luis@hltelecom.cl' );
-define( 'DISPLAY_DEBUG', true ); 
+define( 'DISPLAY_DEBUG', true );
 require_once( 'db.php' );
 ?>
